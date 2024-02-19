3ba5c5645a0c43665f38609ee1f27c98424d23ea
Pablo Diosquez <pablodiosquez24@gmail.com>
2024-02-19 10:28:15 -0300

    Added workbook 2.5

--- workbook 2.5/src/main/java/com/ltp/workbook/WorkbookController.java
+++ workbook 2.5/src/main/java/com/ltp/workbook/WorkbookController.java
@@ -4,13 +4,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+
 @Controller
 public class WorkbookController {
 
     @GetMapping(value="/")
-    public String getSign(Model model) {
-        model.addAttribute("speed", 45);
-        return "sign";
+    public String getMethodName(Model model) {
+        model.addAttribute("painting", "starry-night");
+        return "painting";
     }
     
 }
