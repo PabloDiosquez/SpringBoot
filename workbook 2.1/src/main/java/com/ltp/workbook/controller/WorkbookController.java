1b502323d10e443601d9bf101620ccbe339c88e5
Pablo Diosquez <pablodiosquez24@gmail.com>
2024-02-18 22:17:01 -0300

    update of workbook 2.1 - workbook 2.2

--- workbook 2.1/src/main/java/com/ltp/workbook/controller/WorkbookController.java
+++ workbook 2.1/src/main/java/com/ltp/workbook/controller/WorkbookController.java
@@ -1,12 +1,16 @@
 package com.ltp.workbook.controller;
 
+import com.ltp.workbook.Show;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @org.springframework.stereotype.Controller
 public class WorkbookController {
 
     @GetMapping(path="/")
-    public String getShows(){
+    public String getShows(Model model){
+        Show show = new Show("Breaking Bad", "Ozymandias", 10.0);
+        model.addAttribute(show);
         return "shows";
     }
 }
