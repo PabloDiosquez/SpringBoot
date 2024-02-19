8a6720d044a8b05686233aaea27f9628564f25dc
Pablo Diosquez <pablodiosquez24@gmail.com>
2024-02-19 08:22:00 -0300

    update of workbook 2.1 - workbook 2.2

--- workbook 2.1/src/main/java/com/ltp/workbook/controller/WorkbookController.java
+++ workbook 2.1/src/main/java/com/ltp/workbook/controller/WorkbookController.java
@@ -9,8 +9,11 @@ public class WorkbookController {
 
     @GetMapping(path="/")
     public String getShows(Model model){
-        Show show = new Show("Breaking Bad", "Ozymandias", 10.0);
-        model.addAttribute("show", show);
+        model.addAttribute("show1", new Show("Breaking Bad", "Ozymandias", 10.0));
+        model.addAttribute("show2", new Show("Attack On Titan", "Hero", 9.9));
+        model.addAttribute("show3", new Show("Attack On Titan", "Perfect Game", 9.9));
+        model.addAttribute("show4", new Show("Star Wars: The Clone Wars", "Victory and Death", 9.9));
+        model.addAttribute("show5", new Show("Mr. Robot", "407 Proxy Authentication Required", 9.9));
         return "shows";
     }
 }
