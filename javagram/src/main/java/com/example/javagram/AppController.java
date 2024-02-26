d03d10285a16b253b81efcf2c27b7a562c009b7f
Pablo Diosquez <pablodiosquez24@gmail.com>
2024-02-26 10:37:29 -0300

    Added workbook 3.1

--- javagram/src/main/java/com/example/javagram/AppController.java
+++ javagram/src/main/java/com/example/javagram/AppController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class AppController {
     @GetMapping(path="/")
     public String getForm(Model model){
-        return "sign-up";
+        return "sign.up";
     }
 
     @GetMapping(path="/result")
