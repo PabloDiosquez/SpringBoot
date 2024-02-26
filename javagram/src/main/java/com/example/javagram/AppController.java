8127d81d9822454a65edafaca989620402b47609
Pablo Diosquez <pablodiosquez24@gmail.com>
2024-02-26 11:12:16 -0300

    Added workbook 3.1

--- javagram/src/main/java/com/example/javagram/AppController.java
+++ javagram/src/main/java/com/example/javagram/AppController.java
@@ -3,6 +3,7 @@ package com.example.javagram;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class AppController {
@@ -14,5 +15,11 @@ public class AppController {
     @GetMapping(path="/result")
     public String getResult(){
         return "result";
+    }
+
+    @PostMapping(path="/submitItem")
+    public String handleSubmit(User user){
+
+        return "redirect:/result";
     }
 }
--- javagram/src/main/java/com/example/javagram/User.java
+++ javagram/src/main/java/com/example/javagram/User.java
@@ -1,12 +1,24 @@
 package com.example.javagram;
 
-import java.util.Date;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
 
+import java.util.Date;
 public class User {
+    @NotBlank(message="First name cannot be blank")
+    @Size(min=2, message="First name is too short")
     private String firstname;
+    @NotBlank(message="Last name cannot be blank")
+    @Size(min=2, message="Last name is too short")
     private String lastname;
+    @NotBlank(message="Username cannot be blank" )
+    @Size(min=7, message="Username is too short")
     private String username;
+    @Email
     private String email;
+    @Past
     private Date dateOfBirth;
 
     public User() {
