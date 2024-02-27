d1a884baf7d047ced023fd99363e94fd69faa16e
Pablo Diosquez <pablodiosquez24@gmail.com>
2024-02-26 21:32:12 -0300

    Added workbook 3.1

--- grade-submission-validation/src/main/java/com/ltp/gradesubmission/Grade.java
+++ grade-submission-validation/src/main/java/com/ltp/gradesubmission/Grade.java
@@ -11,6 +11,7 @@ public class Grade {
     private String name;
     @NotBlank(message = "Subject cannot be blank")
     private String subject;
+    @Score(message="Score must be a letter grade")
     private String score;
     private String id;
 
