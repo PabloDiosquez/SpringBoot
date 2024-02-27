4f7fa936823b134543e8a9b89cc943bc0e33fb20
Pablo Diosquez <pablodiosquez24@gmail.com>
2024-02-27 09:58:46 -0300

    Added Field Validation - Challenge

--- global-superstore-revisited/src/main/java/com/ltp/globalsuperstore/StoreController.java
+++ global-superstore-revisited/src/main/java/com/ltp/globalsuperstore/StoreController.java
@@ -24,12 +24,14 @@ public class StoreController {
     public String getForm(Model model, @RequestParam(required = false) String id) {
         int index = getIndexFromId(id);
         model.addAttribute("item", index == Constants.NOT_FOUND ? new Item() : items.get(index));
-        model.addAttribute("categories", Constants.CATEGORIES);
         return "form";
     }
 
     @PostMapping("/submitItem")
     public String handleSubmit(@Valid Item item, BindingResult result, RedirectAttributes redirectAttributes) {
+        if(item.getPrice() < item.getDiscount()){
+            result.reject("", "The price cannot be less than the discount");
+        }
         if(result.hasErrors()) return "form";
         int index = getIndexFromId(item.getId());
         String status = Constants.SUCCESS_STATUS;
