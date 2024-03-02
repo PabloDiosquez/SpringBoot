0bd89c71ba934e153675ed919fb4d9fa68efaa12
Pablo Diosquez <pablodiosquez24@gmail.com>
2024-03-02 12:37:33 -0300

    Added courses

--- courses/src/main/java/com/courses/app/model/Content.java
+++ courses/src/main/java/com/courses/app/model/Content.java
@@ -1,4 +1,22 @@
 package com.courses.app.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter @Setter
+@Entity
 public class Content {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private int idContent;
+    private String contentName;
+    private String description;
 }
