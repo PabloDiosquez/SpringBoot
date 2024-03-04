366d6ecf9444b8ad83082d826bc3de3de1410c5b
Pablo Diosquez <pablodiosquez24@gmail.com>
2024-03-04 13:42:19 -0300

    Updated 'veterinary' project

--- veterinary/src/main/java/com/clinic/veterinary/model/Owner.java
+++ veterinary/src/main/java/com/clinic/veterinary/model/Owner.java
@@ -1,17 +1,25 @@
 package com.clinic.veterinary.model;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.List;
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter @Setter
+@Entity
 public class Owner {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
     private int ownerId;
     private String licenseNumber;
     private String firstname;
     private String lastname;
     private String tel;
+    @OneToMany(mappedBy="owner")
+    private List<Pet> pets;
 }
--- veterinary/src/main/java/com/clinic/veterinary/model/Pet.java
+++ veterinary/src/main/java/com/clinic/veterinary/model/Pet.java
@@ -1,5 +1,6 @@
 package com.clinic.veterinary.model;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,10 +9,16 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter @Setter
+@Entity
 public class Pet {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
     private int petId;
     private String name;
     private String specie;
     private String breed;
     private String color;
+    @ManyToOne
+    @JoinColumn(name="ownerId")
+    private Owner owner;
 }
