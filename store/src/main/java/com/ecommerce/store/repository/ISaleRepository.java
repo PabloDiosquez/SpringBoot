5ebf35433516e1f256eabc7d1a2c58c0a76d8946
Pablo Diosquez <pablodiosquez24@gmail.com>
2024-03-05 14:09:25 -0300

    Updated 'store' project

---
+++ store/src/main/java/com/ecommerce/store/repository/ISaleRepository.java
@@ -0,0 +1,9 @@
+package com.ecommerce.store.repository;
+
+import com.ecommerce.store.model.Sale;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ISaleRepository extends JpaRepository<Sale, Integer> {
+}
