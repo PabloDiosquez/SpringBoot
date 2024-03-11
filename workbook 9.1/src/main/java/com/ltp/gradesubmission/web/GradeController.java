77a195bc846b9759c56d010a04b01cb9a599151a
Pablo Diosquez <pablodiosquez24@gmail.com>
2024-03-11 13:14:06 -0300

    Updated 'sql-database -- workbook 9.2' project

--- workbook 9.1/src/main/java/com/ltp/gradesubmission/web/GradeController.java
+++ workbook 9.1/src/main/java/com/ltp/gradesubmission/web/GradeController.java
@@ -1,12 +1,28 @@
 package com.ltp.gradesubmission.web;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.ltp.gradesubmission.entity.Grade;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
 @RequestMapping("/grade")
 public class GradeController {
-    
+
+    @GetMapping(path="/student/{studentId}/course/{courseId}")
+    public ResponseEntity<Grade> getGrade(@PathVariable Long studentId, @PathVariable Long courseId){
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PostMapping(path="/student/{studentId}/course/{courseId}")
+    public ResponseEntity<Grade> saveGrade(@PathVariable Long studentId, @PathVariable Long courseId, @RequestBody Grade grade){
+        return new ResponseEntity<>(grade, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping(path="/student/{studentId}/course/{courseId}")
+    public ResponseEntity<HttpStatus> deleteGrade(@PathVariable Long studentId, @PathVariable Long courseId){
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 
 }
