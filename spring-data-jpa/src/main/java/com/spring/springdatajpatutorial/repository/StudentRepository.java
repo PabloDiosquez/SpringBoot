40af275c73047742c6f3817a6a92d53ba214f187
Pablo Diosquez <pablodiosquez24@gmail.com>
2024-03-11 21:50:07 -0300

    Updated 'spring-data-jpa' project

---
+++ spring-data-jpa/src/main/java/com/spring/springdatajpatutorial/repository/StudentRepository.java
@@ -0,0 +1,7 @@
+package com.spring.springdatajpatutorial.repository;
+
+import com.spring.springdatajpatutorial.entity.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudentRepository extends JpaRepository<Student, Integer> {
+}
---
+++ spring-data-jpa/src/test/java/com/spring/springdatajpatutorial/repository/StudentRepositoryTest.java
@@ -0,0 +1,38 @@
+package com.spring.springdatajpatutorial.repository;
+
+import com.spring.springdatajpatutorial.entity.Student;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+class StudentRepositoryTest {
+    @Autowired
+    private StudentRepository studentRepository;
+
+    @Test
+    public void saveStudent(){
+        Student student = Student.builder()
+                .email("johnconnor@gmail.com")
+                .firstname("John")
+                .lastname("Connor")
+                .GuardianName("Sarah Connor")
+                .GuardianEmail("sarahconnor@gmail.com")
+                .GuardianMobile("555-111-9012")
+                .build();
+
+        studentRepository.save(student);
+    }
+
+    @Test
+    public void printAllStudent(){
+        List<Student> studentList = studentRepository.findAll();
+
+        System.out.println(studentList);
+    }
+}
