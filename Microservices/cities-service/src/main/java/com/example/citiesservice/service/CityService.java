8a434898f56ed033b0512252829e610ac367cbaa
Pablo Diosquez <pablodiosquez24@gmail.com>
2024-03-20 21:38:35 -0300

    'cities-service' project update

--- Microservices/cities-service/src/main/java/com/example/citiesservice/service/CityService.java
+++ Microservices/cities-service/src/main/java/com/example/citiesservice/service/CityService.java
@@ -15,7 +15,7 @@ public class CityService implements ICityService{
     @Autowired
     private ICityAPIClient cityAPIClient;
 
-    private List<City> cities = new ArrayList<>();
+    private final List<City> cities = new ArrayList<>();
 
     @Override
     public CityDTO getCitiesHotels(String name, String country) {
@@ -25,15 +25,31 @@ public class CityService implements ICityService{
         return new CityDTO(city, hotels);
     }
 
-
-
     private City getCityByNameAndCountry(String name, String country){
+        loadCities();
         for (City city: cities) {
             if(city.getName().equals(name) && city.getCountry().equals(country)){
                 return city;
             }
         }
         return null;
+    }
+
+    private void createException() throws Exception {
+        throw new Exception();
+    }
+
+    private void loadCities(){
+        cities.add(new City(1, "Tokyo", "", "Japan", "Asia"));
+        cities.add(new City(2, "New York City", "New York", "USA", "North America"));
+        cities.add(new City(3, "London", "", "UK", "Europe"));
+        cities.add(new City(4, "Paris", "", "France", "Europe"));
+        cities.add(new City(5, "Beijing", "", "China", "Asia"));
+        cities.add(new City(6, "Sydney", "", "Australia", "Australia"));
+        cities.add(new City(7, "Rio de Janeiro", "", "Brazil", "South America"));
+        cities.add(new City(8, "Cairo", "", "Egypt", "Africa"));
+        cities.add(new City(9, "Moscow", "", "Russia", "Europe"));
+        cities.add(new City(10, "Mexico City", "", "Mexico", "North America"));
     }
 
 }
--- Microservices/hotels-service/src/main/java/com/example/hotelsservice/web/HotelController.java
+++ Microservices/hotels-service/src/main/java/com/example/hotelsservice/web/HotelController.java
@@ -1,11 +1,23 @@
 package com.example.hotelsservice.web;
 
+import com.example.hotelsservice.model.Hotel;
 import com.example.hotelsservice.service.IHotelService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequestMapping(path = "/hotels")
 public class HotelController {
     @Autowired
     private IHotelService hotelService;
+
+    @GetMapping(path = "/{cityId}")
+    public List<Hotel> getHotelsByCityId(@PathVariable int cityId){
+        return hotelService.getHotelsByCityId(cityId);
+    }
 }
