a02e6cb34231ad645b52a64d121331ccfa75f852
Pablo Diosquez <pablodiosquez24@gmail.com>
2024-03-20 21:46:24 -0300

    'cities-service' project update

--- Microservices/cities-service/src/main/java/com/example/citiesservice/web/CityController.java
+++ Microservices/cities-service/src/main/java/com/example/citiesservice/web/CityController.java
@@ -1,11 +1,21 @@
 package com.example.citiesservice.web;
 
+import com.example.citiesservice.dto.CityDTO;
 import com.example.citiesservice.service.ICityService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(path = "/cities")
 public class CityController {
     @Autowired
     private ICityService cityService;
+
+    @GetMapping(path = "/hotels/")
+    public CityDTO getCityAndHotels(@RequestParam String cityName, @RequestParam String country){
+        return cityService.getCitiesHotels(cityName, country);
+    }
 }
