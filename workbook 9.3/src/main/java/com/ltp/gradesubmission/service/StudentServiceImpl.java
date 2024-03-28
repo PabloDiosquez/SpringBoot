fce8a12f0aa41cb5cdccfb79c07c05a6afb6a630
Pablo Diosquez <pablodiosquez24@gmail.com>
2024-03-28 13:02:27 -0300

    'workbook 9.3' was updated to the repository

--- workbook 9.3/src/main/java/com/ltp/gradesubmission/service/StudentServiceImpl.java
+++ workbook 9.3/src/main/java/com/ltp/gradesubmission/service/StudentServiceImpl.java
@@ -3,27 +3,35 @@ package com.ltp.gradesubmission.service;
 import java.util.List;
 
 import com.ltp.gradesubmission.entity.Student;
+import com.ltp.gradesubmission.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class StudentServiceImpl implements StudentService {
+    @Autowired
+    private StudentRepository studentRepository;
 
     @Override
     public Student getStudent(Long id) {
-        return null;
+        return studentRepository.findById(id).orElse(null);
     }
 
     @Override
     public Student saveStudent(Student student) {
-        return null;
+        assert student != null;
+        studentRepository.save(student);
+        return student;
     }
 
     @Override
-    public void deleteStudent(Long id) {        
+    public void deleteStudent(Long id) {
+        studentRepository.deleteById(id);
     }
 
     @Override
     public List<Student> getStudents() {
-        return null;
+        return (List<Student>) studentRepository.findAll();
     }
-
 
 }
