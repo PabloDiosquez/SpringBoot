f5f3590d401af5ad6a9f56fc16858128f0704fdb
Pablo Diosquez <pablodiosquez24@gmail.com>
2024-03-29 16:20:06 -0300

    'workbook 9.3' was updated to the repository

--- workbook 9.3/src/main/java/com/ltp/gradesubmission/web/GradeController.java
+++ workbook 9.3/src/main/java/com/ltp/gradesubmission/web/GradeController.java
@@ -3,6 +3,7 @@ package com.ltp.gradesubmission.web;
 import java.util.List;
 
 import com.ltp.gradesubmission.service.GradeService;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,11 +17,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ltp.gradesubmission.entity.Grade;
-
+@AllArgsConstructor
 @RestController
 @RequestMapping("/grade")
 public class GradeController {
-    @Autowired
+
     private GradeService gradeService;
     
     @GetMapping("/student/{studentId}/course/{courseId}")
@@ -35,7 +36,7 @@ public class GradeController {
 
     @PutMapping("/student/{studentId}/course/{courseId}")
     public ResponseEntity<Grade> updateGrade(@RequestBody Grade grade, @PathVariable Long studentId, @PathVariable Long courseId) {
-        return new ResponseEntity<>(grade, HttpStatus.OK);
+        return new ResponseEntity<>(gradeService.updateGrade(grade.getScore(), studentId, courseId), HttpStatus.OK);
     }
 
     @DeleteMapping("/student/{studentId}/course/{courseId}")
