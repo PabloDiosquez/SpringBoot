18af72ba307f8e085b9054dd3b2ac2e83f94845b
Pablo Diosquez <pablodiosquez24@gmail.com>
2024-03-29 13:19:33 -0300

    'workbook 9.3' was updated to the repository

--- workbook 9.3/src/main/java/com/ltp/gradesubmission/entity/Course.java
+++ workbook 9.3/src/main/java/com/ltp/gradesubmission/entity/Course.java
@@ -1,42 +1,30 @@
 package com.ltp.gradesubmission.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter @Setter
+@Entity
+@Table(name = "course")
 public class Course {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
     private Long id;
+
+    @Column(name = "subject", nullable = false)
     private String subject;
+
+    @Column(name = "code", nullable = false)
     private String code;
+
+    @Column(name = "description", nullable = false)
     private String description;
-
-    public Long getId() {
-        return this.id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getSubject() {
-        return this.subject;
-    }
-
-    public void setSubject(String subject) {
-        this.subject = subject;
-    }
-
-    public String getCode() {
-        return this.code;
-    }
-
-    public void setCode(String code) {
-        this.code = code;
-    }
-
-    public String getDescription() {
-        return this.description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 
 }
