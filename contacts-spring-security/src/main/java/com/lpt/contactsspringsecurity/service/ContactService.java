68ee520eb953c1844dc783ec2796fbd021c99952
Pablo Diosquez <pablodiosquez24@gmail.com>
2024-04-02 14:31:08 -0300

    'contacts-spring-security' was updated to the repository

--- contacts-spring-security/src/main/java/com/lpt/contactsspringsecurity/service/ContactService.java
+++ contacts-spring-security/src/main/java/com/lpt/contactsspringsecurity/service/ContactService.java
@@ -1,4 +1,13 @@
 package com.lpt.contactsspringsecurity.service;
 
+import com.lpt.contactsspringsecurity.pojo.Contact;
+
+import java.util.List;
+
 public interface ContactService {
+
+    List<Contact> getContacts();
+    Contact getContactById(String id);
+    void saveContact(Contact contact);
+    void deleteContact(String id);
 }
--- contacts-spring-security/src/main/java/com/lpt/contactsspringsecurity/service/ContactServiceImpl.java
+++ contacts-spring-security/src/main/java/com/lpt/contactsspringsecurity/service/ContactServiceImpl.java
@@ -1,4 +1,43 @@
 package com.lpt.contactsspringsecurity.service;
 
-public class ContactServiceImpl {
+import com.lpt.contactsspringsecurity.exception.ContactNotFoundException;
+import com.lpt.contactsspringsecurity.pojo.Contact;
+import com.lpt.contactsspringsecurity.repository.ContactRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.IntStream;
+
+@AllArgsConstructor
+@Service
+public class ContactServiceImpl implements ContactService{
+
+    private ContactRepository contactRepository;
+    @Override
+    public List<Contact> getContacts() {
+        return contactRepository.getContacts();
+    }
+
+    @Override
+    public Contact getContactById(String id) {
+        return contactRepository.getContact(findIndexById(id));
+    }
+
+    @Override
+    public void saveContact(Contact contact) {
+        contactRepository.saveContact(contact);
+    }
+
+    @Override
+    public void deleteContact(String id) {
+        contactRepository.deleteContact(findIndexById(id));
+    }
+
+    private int findIndexById(String id){
+        return IntStream.range(0, contactRepository.getContacts().size())
+                .filter(index -> contactRepository.getContacts().get(index).getId().equals(id))
+                .findFirst()
+                .orElseThrow(() -> new ContactNotFoundException(id));
+    }
 }
