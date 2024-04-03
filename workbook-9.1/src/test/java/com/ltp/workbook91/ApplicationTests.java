795b1dc1692c9341390dc1c057571866353fba41
Pablo Diosquez <pablodiosquez24@gmail.com>
2024-04-03 16:24:30 -0300

    'workbook-9.1' was updated to the repository

---
+++ workbook-9.1/src/test/java/com/ltp/workbook91/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ltp.workbook91;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
