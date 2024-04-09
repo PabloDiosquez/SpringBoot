8e619778578824d032f4c52d20ba164ef92c8db7
Pablo Diosquez <pablodiosquez24@gmail.com>
2024-04-09 14:32:30 -0300

    'auth-jwt' was updated to the repository

---
+++ 10. spring-security/auth-jwt/grade-submission/src/main/java/com/ltp/gradesubmission/repository/CourseRepository.java
@@ -0,0 +1,6 @@
+package com.ltp.gradesubmission.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CourseRepository extends CrudRepository<CourseRepository, Long> {
+}
