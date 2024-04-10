130a7f2d49e0ae3cc663c548f6deb3330eaf176c
Pablo Diosquez <pablodiosquez24@gmail.com>
2024-04-10 17:01:30 -0300

    'auth-jwt' was updated to the repository

--- 10. spring-security/auth-jwt/grade-submission/src/main/java/com/ltp/gradesubmission/web/CourseController.java
+++ 10. spring-security/auth-jwt/grade-submission/src/main/java/com/ltp/gradesubmission/web/CourseController.java
@@ -1,4 +1,53 @@
 package com.ltp.gradesubmission.web;
 
+import com.ltp.gradesubmission.entity.Course;
+import com.ltp.gradesubmission.entity.Student;
+import com.ltp.gradesubmission.repository.CourseRepository;
+import com.ltp.gradesubmission.service.CourseService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Set;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping(path = "/course")
 public class CourseController {
+
+    private CourseService courseService;
+
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<Course> getCourse(@PathVariable(name = "id") Long id){
+        return new ResponseEntity<>(courseService.getCourse(id), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Course> saveCourse(@RequestBody Course course){
+        return new ResponseEntity<>(courseService.saveCourse(course), HttpStatus.CREATED);
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity<HttpStatus> deleteCourse(@PathVariable(name = "id") Long id){
+        courseService.deleteCourse(id);
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @PostMapping(path = "/{courseId}/student/{studentId}")
+    public ResponseEntity<Course> enrollStudentToCourse(@PathVariable(name = "courseId") Long courseId,
+                                                     @PathVariable(name = "studentId") Long studentId){
+        return new ResponseEntity<>(courseService.addStudentToCourse(studentId, courseId), HttpStatus.OK);
+    }
+    @GetMapping(path = "/all")
+    public ResponseEntity<List<Course>> getCourses(){
+        return new ResponseEntity<>(courseService.getCourses(), HttpStatus.OK);
+    }
+
+    @GetMapping(path = "/{id}/students")
+    public ResponseEntity<Set<Student>> getEnrollStudents(@PathVariable(name = "id") Long id){
+        return new ResponseEntity<>(courseService.getEnrolledStudents(id), HttpStatus.OK);
+    }
+
 }
