5d30048d2c68856a6ec96c7bb583ea599e0a1e92
Pablo Diosquez <pablodiosquez24@gmail.com>
2024-04-10 17:12:52 -0300

    'auth-jwt' was updated to the repository

--- 10. spring-security/auth-jwt/grade-submission/src/main/java/com/ltp/gradesubmission/web/UserController.java
+++ 10. spring-security/auth-jwt/grade-submission/src/main/java/com/ltp/gradesubmission/web/UserController.java
@@ -1,4 +1,27 @@
 package com.ltp.gradesubmission.web;
 
+import com.ltp.gradesubmission.entity.User;
+import com.ltp.gradesubmission.service.UserService;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping(path = "/user")
 public class UserController {
+
+    private UserService userService;
+    @GetMapping("/{id}")
+    public ResponseEntity<String> findById(@PathVariable Long id) {
+        return new ResponseEntity<>(userService.getUser(id).getUsername(), HttpStatus.OK);
+    }
+
+    @PostMapping(path = "/register")
+    public ResponseEntity<HttpStatus> saveUser(@Valid @RequestBody User user){
+        userService.saveUser(user);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
 }
