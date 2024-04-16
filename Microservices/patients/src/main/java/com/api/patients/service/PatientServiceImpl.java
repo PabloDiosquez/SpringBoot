3866b1b5c235574894b56cd7b5ae2a66a9d00036
Pablo Diosquez <pablodiosquez24@gmail.com>
2024-04-16 16:11:27 -0300

    'patients api' was updated to the repository

--- Microservices/patients/src/main/java/com/api/patients/service/PatientServiceImpl.java
+++ Microservices/patients/src/main/java/com/api/patients/service/PatientServiceImpl.java
@@ -7,32 +7,47 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+
 @AllArgsConstructor
 @Service
 public class PatientServiceImpl implements PatientService{
     private PatientRepository patientRepository;
     @Override
     public Patient getPatient(Long id) {
-        return null;
+        return unwrapPatient(patientRepository.findById(id), id);
     }
 
     @Override
     public List<Patient> getAllPatients() {
-        return null;
+        return patientRepository.findAll();
     }
 
     @Override
     public Patient createPatient(Patient patient) {
-        return null;
+        return patientRepository.save(patient);
     }
 
     @Override
     public void deletePatient(Long id) {
-
+        Patient patient = getPatient(id);
+        patientRepository.deleteById(patient.getPatientId());
     }
 
     @Override
     public Patient updatePatient(Long id, Patient patient) {
-        return null;
+        Patient old = getPatient(id);
+        old.setFirstname(patient.getFirstname());
+        old.setLastname(patient.getLastname());
+        old.setBirthDate(patient.getBirthDate());
+        old.setTelephone(patient.getTelephone());
+        return patientRepository.save(old);
+    }
+
+    private Patient unwrapPatient(Optional<Patient> entity, Long id){
+        if(entity.isPresent()){
+            return entity.get();
+        }
+        throw new RuntimeException("The patient with id: '" + id + "' does not exist in our records.");
     }
 }
