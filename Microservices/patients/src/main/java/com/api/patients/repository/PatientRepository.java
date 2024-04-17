910784b468d5963c3ad136d6b55056a4d9e2c501
Pablo Diosquez <pablodiosquez24@gmail.com>
2024-04-17 17:56:53 -0300

    'patients api' was updated to the repository

--- Microservices/patients/src/main/java/com/api/patients/repository/PatientRepository.java
+++ Microservices/patients/src/main/java/com/api/patients/repository/PatientRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface PatientRepository extends JpaRepository<Patient, Long> {
-    @Query("SELECT p FROM Patient p WHERE p.licenseNumber =: licenseNumber")
+    @Query("SELECT p FROM Patient p WHERE p.licenseNumber =:licenseNumber")
     Patient findByLicenseNumber(String licenseNumber);
 }
