cb34688af0f5e768d0604c699462e8e33b80a7dd
Pablo Diosquez <pablodiosquez24@gmail.com>
2024-04-23 09:25:53 -0300

    'users-sv' was updated to the repository

---
+++ Microservices/users-service/src/main/java/com/example/usersservice/model/User.java
@@ -0,0 +1,27 @@
+package com.example.usersservice.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter @Setter
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastname;
+
+    @Column(name = "cellphone", nullable = false)
+    private String cellphone;
+}
