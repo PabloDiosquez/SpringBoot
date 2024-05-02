24780a2f6e43b3de65ae5d46ed2215c32e72c91a
Pablo Diosquez <pablodiosquez24@gmail.com>
2024-05-02 16:18:12 -0300

    'pokemon-api' was updated to the repository

--- Unit Testing/api/src/test/java/com/pokemonreview/api/ApiApplicationTests.java
+++ Unit Testing/api/src/test/java/com/pokemonreview/api/ApiApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.pokemonapi;
+package com.pokemonreview.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class PokemonApiApplicationTests {
+class ApiApplicationTests {
 
 	@Test
 	void contextLoads() {
