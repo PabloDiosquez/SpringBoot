0a2a863f63091feab8415b87bacbb8d9e3166a8c
Pablo Diosquez <pablodiosquez24@gmail.com>
2024-05-06 11:04:00 -0300

    'pokemon-api' was updated to the repository

---
+++ Unit Testing/api/src/main/java/com/pokemonreview/api/models/Pokemon.java
@@ -0,0 +1,22 @@
+package com.pokemonreview.api.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+public class Pokemon {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "type", nullable = false)
+    private String type;
+}
---
+++ Unit Testing/api/src/main/java/com/pokemonreview/api/models/Review.java
@@ -0,0 +1,25 @@
+package com.pokemonreview.api.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+public class Review {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "string")
+    private String content;
+
+    @Column(name = "stars")
+    private int stars;
+}
