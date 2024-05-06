57a8299d014ad9b1e43ef9f2e24ae54adff6fc81
Pablo Diosquez <pablodiosquez24@gmail.com>
2024-05-06 09:28:50 -0300

    'eureka-sv' was updated to the repository

---
+++ Microservices/Appliances Store/eureka-sv/src/main/java/com/store/eurekasv/EurekaSvApplication.java
@@ -0,0 +1,13 @@
+package com.store.eurekasv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaSvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaSvApplication.class, args);
+	}
+
+}
---
+++ Microservices/Appliances Store/eureka-sv/src/test/java/com/store/eurekasv/EurekaSvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.store.eurekasv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaSvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
