f4004d7f6fa358aeddbf723c39658dab3a02ec03
Pablo Diosquez <pablodiosquez24@gmail.com>
2024-05-13 11:15:12 -0300

    'pokemon-api'was updated

--- Unit Testing/api/src/main/java/com/pokemonreview/api/security/SecurityConfig.java
+++ Unit Testing/api/src/main/java/com/pokemonreview/api/security/SecurityConfig.java
@@ -1,9 +1,15 @@
 package com.pokemonreview.api.security;
 
+import jakarta.servlet.DispatcherType;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -15,10 +21,28 @@ public class SecurityConfig {
         httpSecurity
                 .csrf().disable()
                 .authorizeRequests()
+                .dispatcherTypeMatchers(HttpMethod.GET).permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .httpBasic();
 
         return httpSecurity.build();
+    }
+
+    @Bean
+    public UserDetailsService users(){
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password("admin_pass")
+                .roles("ADMIN")
+                .build();
+
+        UserDetails user = User.builder()
+                .username("user")
+                .password("user_pass")
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(admin, user);
     }
 }
