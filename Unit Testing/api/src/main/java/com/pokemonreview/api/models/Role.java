c3d53f6f8125c835678a7074c5bf7feef69c0998
Pablo Diosquez <pablodiosquez24@gmail.com>
2024-05-13 13:46:35 -0300

    'pokemon-api'was updated

---
+++ Unit Testing/api/src/main/java/com/pokemonreview/api/models/Role.java
@@ -0,0 +1,19 @@
+package com.pokemonreview.api.models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter @Setter
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "role", nullable = false)
+    private String name;
+}
--- Unit Testing/api/src/main/java/com/pokemonreview/api/models/UserEntity.java
+++ Unit Testing/api/src/main/java/com/pokemonreview/api/models/UserEntity.java
@@ -4,6 +4,8 @@ import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @NoArgsConstructor
 @Data
 @Entity
@@ -18,4 +20,12 @@ public class UserEntity {
 
     @Column(name = "password", nullable = false)
     private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "users_roles",
+            joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id")
+    )
+    private List<Role> roles;
 }
