276afd69a1026840d139680a70ff6f9c2ba091d8
Pablo Diosquez <pablodiosquez24@gmail.com>
2024-05-15 19:00:20 -0300

    'Spring Data JPA'was updated

---
+++ Spring Data JPA/spring-data-jpa/src/main/java/com/example/spring/data/jpa/entity/Student.java
@@ -0,0 +1,50 @@
+package com.example.spring.data.jpa.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Builder
+@Table(
+        name = "students",
+        uniqueConstraints = @UniqueConstraint(
+                columnNames = {"firstname", "lastname"}
+        )
+)
+public class Student {
+    @Id
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
+    private int id;
+
+    @Column(nullable = false)
+    private String firstname;
+
+    @Column(nullable = false)
+    private String lastname;
+
+    @Column(name = "email_address", nullable = false)
+    private String email;
+
+    @Column(name = "guardian", nullable = false)
+    private String guardianName;
+
+    @Column(name = "guardian_email")
+    private String guardianEmail;
+
+    @Column(name = "guardian_mobile")
+    private String guardianMobile;
+}
