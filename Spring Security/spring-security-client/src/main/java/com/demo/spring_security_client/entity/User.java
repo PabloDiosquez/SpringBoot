2f6761cc7cbccd504bcafee1d03b70286e956feb
Pablo Diosquez <pablodiosquez24@gmail.com>
2024-05-23 16:51:35 -0300

    'Spring Security'was updated

---
+++ Spring Security/spring-security-client/src/main/java/com/demo/spring_security_client/entity/User.java
@@ -0,0 +1,29 @@
+package com.demo.spring_security_client.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+@Entity
+@Table(
+        name = "users",
+        uniqueConstraints = @UniqueConstraint(columnNames = {"firstname", "lastname"})
+)
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String firstname;
+    private String lastname;
+    private String email;
+    @Column(length = 60)
+    private String password;
+    private String role;
+    private boolean enabled = false;
+}
