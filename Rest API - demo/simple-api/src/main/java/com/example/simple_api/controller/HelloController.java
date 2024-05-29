e5c53dfc507ddf45a29e2fbed3f01b53000884d5
Pablo Diosquez <pablodiosquez24@gmail.com>
2024-05-29 09:30:58 -0300

    'Simple API Rest' was updated

--- Rest API - demo/simple-api/src/main/java/com/example/simple_api/controller/HelloController.java
+++ Rest API - demo/simple-api/src/main/java/com/example/simple_api/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.example.simple_api.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
@@ -9,15 +10,18 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class HelloController {
 
+    @Value("${greeting.message}")
+    private String message;
+
     // Define a route to handle HTTP GET requests at the root path ("/")
-    @RequestMapping(path = "/", method = RequestMethod.GET)
+    @RequestMapping(path = "/message", method = RequestMethod.GET)
     public String sayHello() {
         // Return a simple greeting message
-        return "Hello everyone!!";
+        return message;
     }
 
     // Define a route to handle HTTP GET requests at paths like "/{name}"
-    @GetMapping(path = "/{name}")
+    @GetMapping(path = "/message/{name}")
     public String sayHello(@PathVariable(name = "name", required = false) String name) {
         // Return a personalized greeting message including the provided name
         return "Hello " + name + "!";
