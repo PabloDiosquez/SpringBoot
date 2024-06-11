b84d87e7e0d9f1f1963d0d9b76737b7a5b834dcc
Pablo Diosquez <pablodiosquez24@gmail.com>
2024-06-11 15:14:08 -0300

    'app' was updated

--- Spring Data JPA/app/src/main/java/com/meta/app/controller/FirstController.java
+++ Spring Data JPA/app/src/main/java/com/meta/app/controller/FirstController.java
@@ -14,7 +14,6 @@ public class FirstController {
     public ResponseEntity<String> sayHi(){
         return new ResponseEntity<>("Hi from my first controller! :)", HttpStatus.OK);
     }
-
     @PostMapping(path = "/post")
     public ResponseEntity<String> post(@RequestBody String message){
         return new ResponseEntity<>("Request accepted - Message: " + message, HttpStatus.ACCEPTED);
@@ -27,6 +26,15 @@ public class FirstController {
     @PostMapping(path = "/post-order-record")
     public ResponseEntity<String> postRecorde(@RequestBody OrderRecord orderRecord){
         return new ResponseEntity<>(orderRecord.toString(), HttpStatus.OK);
+    }
+
+    @GetMapping(path = "/hi/{user-name}")
+    public ResponseEntity<String> sayHi(@PathVariable(name = "user-name") String userName){
+        return new ResponseEntity<>("Hi " + userName + "! :)", HttpStatus.OK);
+    }
+    @GetMapping(path = "/hi/user")
+    public ResponseEntity<String> sayHiParam(@RequestParam(name = "user-name") String userName){
+        return new ResponseEntity<>("Hi " + userName + "! :)", HttpStatus.OK);
     }
 
 }
