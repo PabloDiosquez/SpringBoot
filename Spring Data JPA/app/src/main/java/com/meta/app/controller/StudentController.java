f73557bf04dd6e942a919f82c57e30ecffcb73b7
Pablo Diosquez <pablodiosquez24@gmail.com>
2024-06-18 17:01:11 -0300

    'app' was updated

--- Spring Data JPA/app/src/main/java/com/meta/app/controller/StudentController.java
+++ Spring Data JPA/app/src/main/java/com/meta/app/controller/StudentController.java
@@ -5,18 +5,29 @@ import com.meta.app.repository.StudentRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @AllArgsConstructor
 @RestController
+@RequestMapping(path = "/students")
 public class StudentController {
     private StudentRepository studentRepository;
 
-    @GetMapping(path = "/students")
+    @GetMapping(path = "/")
     public ResponseEntity<List<Student>> getStudents(){
         return new ResponseEntity<>(studentRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<Student> getStudent(@PathVariable(name = "id") int id){
+        return new ResponseEntity<>(studentRepository.findById(id).orElseThrow(() -> new RuntimeException("ID not found in our records")),
+                HttpStatus.OK);
+    }
+
+    @PostMapping(path = "/")
+    public ResponseEntity<Student> saveStudent(@RequestBody Student student){
+        return new ResponseEntity<>(studentRepository.save(student), HttpStatus.CREATED);
     }
 }
